ded0a7e3d15caeb4991c11501ac612a32b933419
Alexander Ignatov <yalishanda@abv.bg>
2020-10-29 12:06:07 +0200

    Implement successors

--- solution.py
+++ solution.py
@@ -1,20 +1,57 @@
 """62136"""
 
+from copy import deepcopy
+
 
 class Coordinate:
     def __init__(self, row, col):
         self.row = row
         self.col = col
 
+    def __repr__(self):
+        return f"Coordinate({self.row}, {self.col})"
+
     def __eq__(self, other):
         """Return self == other."""
         return self.row == other.row and self.col == other.col
+
+    def __add__(self, other):
+        """Return self + other."""
+        return Coordinate(self.row + other.row, self.col + other.col)
+
+    def is_valid(self, limit):
+        """
+        Return `True` if the coordinate is valid for a matrix of size `limit`.
+        """
+        return 0 <= self.row < limit and 0 <= self.col < limit
 
     def manhattan_distance(self, other):
         """
         Return the Manhattan distance between self and other.
         """
         return abs(self.row - other.row) + abs(self.col - other.col)
+
+
+class Action:
+    def __init__(self, name, zero_delta):
+        """
+        Initialize an action with a name and the difference in the
+        coordinates of the 0 element.
+        """
+        self.name = name
+        self.delta = zero_delta
+
+    def __repr__(self):
+        return f"Action({self.name}, {self.delta})"
+
+
+class Actions:
+    ALL = [
+        Action("left", Coordinate(0, +1)),
+        Action("right", Coordinate(0, -1)),
+        Action("up", Coordinate(+1, 0)),
+        Action("down", Coordinate(-1, 0)),
+    ]
 
 
 class State:
@@ -26,6 +63,9 @@ class State:
         """
         self.matrix = matrix
         self.matrix_size = len(matrix)
+
+    def __repr__(self):
+        return f"State({self.matrix})"
 
     @property
     def coordinates(self):
@@ -48,3 +88,24 @@ class State:
             for (number, coordinate) in self.coordinates.items()
             if number != 0
         )
+
+    def successors(self):
+        """
+        Return all tuples with successor states and
+        the action needed to create the state.
+        """
+        zer_coord = self.coordinates[0]
+        legal_moves = [
+            action for action in Actions.ALL
+            if (zer_coord + action.delta).is_valid(self.matrix_size)
+        ]
+        states = []
+        for move in legal_moves:
+            new_zero = zer_coord + move.delta
+            number_to_move = self.matrix[new_zero.row][new_zero.col]
+            new_matrix = deepcopy(self.matrix)
+            new_matrix[zer_coord.row][zer_coord.col] = number_to_move
+            new_matrix[new_zero.row][new_zero.col] = 0
+            states.append(State(new_matrix))
+
+        return list(zip(states, legal_moves))
--- tests.py
+++ tests.py
@@ -17,7 +17,7 @@ class Tests(unittest.TestCase):
         self.assertEqual(expect, result1)
         self.assertEqual(expect, result2)
 
-    def test_manhattan_distance_equals(self):
+    def test_coordinates_equal(self):
         """Test that two same coordinates are equal."""
         c1 = Coordinate(1, 1)
         c2 = Coordinate(1, 1)
@@ -25,6 +25,32 @@ class Tests(unittest.TestCase):
 
         self.assertEqual(c1, c2)
         self.assertEqual(0, d)
+
+    def test_coordinates_valid(self):
+        """Test the validity of coordinates."""
+        limit = 3
+        c1 = Coordinate(2, 2)
+        c2 = Coordinate(-1, 1)
+        c3 = Coordinate(0, 3)
+
+        v1 = c1.is_valid(limit)
+        v2 = c2.is_valid(limit)
+        v3 = c3.is_valid(limit)
+
+        self.assertTrue(v1)
+        self.assertFalse(v2)
+        self.assertFalse(v3)
+
+
+    def test_coordinates_addition(self):
+        """Test that 2 coordinates add up correctly."""
+        c1 = Coordinate(1, 2)
+        c2 = Coordinate(-1, 1)
+        expect = Coordinate(0, 3)
+
+        result = c1 + c2
+
+        self.assertEqual(expect, result)
 
     def test_state_coordinates_correct(self):
         """Test that `State.coordinates` produces correct coordinates."""
@@ -83,6 +109,105 @@ class Tests(unittest.TestCase):
         result = s.heuristic(g)
 
         self.assertEqual(expect, result)
+
+    def test_successors_two(self):
+        """Test generating two successors."""
+        state_matrix = [
+            [0, 2, 3],
+            [1, 4, 5],
+            [6, 7, 8],
+        ]
+        s = State(state_matrix)
+        expect_matrixes = [
+            [
+                [2, 0, 3],
+                [1, 4, 5],
+                [6, 7, 8],
+            ],
+            [
+                [1, 2, 3],
+                [0, 4, 5],
+                [6, 7, 8],
+            ],
+        ]
+        expect_moves = ["left", "up"]
+
+        result = s.successors()
+
+        self.assertEqual(2, len(result))
+        self.assertEqual(expect_matrixes, [s[0].matrix for s in result])
+        self.assertEqual(expect_moves, [s[1].name for s in result])
+
+    def test_successors_three(self):
+        """Test generating three successors."""
+        state_matrix = [
+            [1, 2, 3],
+            [0, 4, 5],
+            [6, 7, 8],
+        ]
+        s = State(state_matrix)
+        expect_matrixes = [
+            [
+                [1, 2, 3],
+                [4, 0, 5],
+                [6, 7, 8],
+            ],
+            [
+                [1, 2, 3],
+                [6, 4, 5],
+                [0, 7, 8],
+            ],
+            [
+                [0, 2, 3],
+                [1, 4, 5],
+                [6, 7, 8],
+            ],
+        ]
+        expect_moves = ["left", "up", "down"]
+
+        result = s.successors()
+
+        self.assertEqual(3, len(result))
+        self.assertEqual(expect_matrixes, [s[0].matrix for s in result])
+        self.assertEqual(expect_moves, [s[1].name for s in result])
+
+    def test_successors_four(self):
+        """Test generating four successors."""
+        state_matrix = [
+            [1, 2, 3],
+            [4, 0, 5],
+            [6, 7, 8],
+        ]
+        s = State(state_matrix)
+        expect_matrixes = [
+            [
+                [1, 2, 3],
+                [4, 5, 0],
+                [6, 7, 8],
+            ],
+            [
+                [1, 2, 3],
+                [0, 4, 5],
+                [6, 7, 8],
+            ],
+            [
+                [1, 2, 3],
+                [4, 7, 5],
+                [6, 0, 8],
+            ],
+            [
+                [1, 0, 3],
+                [4, 2, 5],
+                [6, 7, 8],
+            ],
+        ]
+        expect_moves = ["left", "right", "up", "down"]
+
+        result = s.successors()
+
+        self.assertEqual(4, len(result))
+        self.assertEqual(expect_matrixes, [s[0].matrix for s in result])
+        self.assertEqual(expect_moves, [s[1].name for s in result])
 
 
 if __name__ == "__main__":
